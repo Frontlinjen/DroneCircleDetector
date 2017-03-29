3757b734817095b4803a24f70d9f73f42dd02e92
Senad Begovic <senadbegovic2500@hotmail.com>
2017-03-29 14:28:57 +0200

    Clean up old code, moved some code from the ring_detector.cpp to .h and deleted a lot of code, which didn't belong to the corresponding class

--- include/ring_detector/Ring_Detector.h
+++ include/ring_detector/Ring_Detector.h
@@ -1,19 +1,49 @@
-/*
- * ring_detector.h
- *
- *  Created on: Mar 29, 2017
- *      Author: senad
- */
+#include <ros/ros.h>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/utility.hpp>
+#include <ros/ros.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <semaphore.h>
+#include <chrono>
+#include <utility>
+#include <math.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/videoio.hpp>
 
-#ifndef INCLUDE_RING_DETECTOR_RING_DETECTOR_H_
-#define INCLUDE_RING_DETECTOR_RING_DETECTOR_H_
+
+template<typename T, int MIN=0, int MAX=100, int RATIO=1>
+void TrackbarCallback(int pos, void * ptr){
+    if(pos < MIN || pos > MAX)
+        return;
+    double ratio = 1/RATIO;
+    T * val = static_cast<T*>(ptr);
+    *val = pos*RATIO;
+}
+
 class Ring_Detector{
-public:
-
 private:
+	const float UPDATE_RATE;
+	int minDist;
+	int minRadius;
+	int maxRadius;
+	int param1;
+	int param2;
 
+public:
+	void processImage( cv_bridge::CvImageConstPtr image);
 
-
-
-#endif /* INCLUDE_RING_DETECTOR_RING_DETECTOR_H_ */
+	Ring_Detector() : UPDATE_RATE(120){
+		cv::namedWindow("Drone feed", 1);
+		cv::createTrackbar("minDist", "Drone feed", &minDist, 300, TrackbarCallback<int>);
+		cv::createTrackbar("param1", "Drone feed", &param1, 300);
+		cv::createTrackbar("param2", "Drone feed", &param2, 300);
+		cv::createTrackbar("minRadius", "Drone feed", &minRadius, 300);
+		cv::createTrackbar("maxRadius", "Drone feed", &maxRadius, 300);
+	}
 };
--- src/ring_detector.cpp
+++ src/ring_detector.cpp
@@ -15,108 +15,15 @@
 #include <math.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/videoio.hpp>
-
-template<typename T, int MIN=0, int MAX=100, int RATIO=1>
-void TrackbarCallback(int pos, void * ptr){
-    if(pos < MIN || pos > MAX)
-        return;
-    double ratio = 1/RATIO;
-    T * val = static_cast<T*>(ptr);
-    *val = pos*RATIO;
-}
-
-#define MEASURE_TIME 1
-
-#ifdef MEASURE_TIME
-#define BEGIN_SCOPE_MEASURE(name)  AutoTimeMeasure mTime(name)
-#else
-#define BEGIN_SCOPE_MEASURE(name) do {} while(0)
-#endif
-
-class AutoTimeMeasure{
-    const char * name;
-    std::chrono::high_resolution_clock::time_point start;
-public:
-    AutoTimeMeasure(const char * c)
-    {
-        name = c;
-        start = std::chrono::high_resolution_clock::now();
-    }
-    ~AutoTimeMeasure(){
-        std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-        printf("%s finished in %fs\n", name, std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()/1000000000.0);
-    }
-};
-
-const float UPDATE_RATE = 120.0;
-sem_t * semaphore;
-cv::VideoWriter * writer;
-
-int minDist = 10;
-int minRadius = 10;
-int maxRadius = 10;
-int param1 = 30;
-int param2 = 150;
-
-void processImage( cv_bridge::CvImageConstPtr image);
-
-void imageCallback(const sensor_msgs::ImageConstPtr & msg)
-{
-	if(sem_trywait(semaphore)!=0)
-		return;
-
-	BEGIN_SCOPE_MEASURE("ImageProcessing");
-	cv_bridge::CvImageConstPtr imagePtr = cv_bridge::toCvShare(msg);
-	processImage(imagePtr);
-	cv::waitKey(1);
-	sem_post(semaphore);
-}
-void SetupRecorder()
-{
-//#if RECORD_STREAM
-    const int width = 1280;
-    const int height = 720;
-    const int fps = 30;
-    const int fourcc = cv::VideoWriter::fourcc('M', 'J', 'P', 'G'); //cap.get(CAP_PROP_FOURCC);
-    writer = new cv::VideoWriter("sample.avi", fourcc, fps, cv::Size(width, height));
-//    #endif
-}
-
-//min, max, step?
+#include "ring_detector/Ring_Detector.h"
 
 
 int main(int argc, char ** argv)
 {
 
-	semaphore = new sem_t();
-	sem_init(semaphore, 0,1);
-	SetupRecorder();
-	cv::namedWindow("Drone feed", 1);
-    cv::createTrackbar("minDist", "Drone feed", &minDist, 300, TrackbarCallback<int>);
-    cv::createTrackbar("param1", "Drone feed", &param1, 300);
-    cv::createTrackbar("param2", "Drone feed", &param2, 300);
-    cv::createTrackbar("minRadius", "Drone feed", &minRadius, 300);
-    cv::createTrackbar("maxRadius", "Drone feed", &maxRadius, 300);
-
-    using namespace image_transport;
-	ros::init(argc, argv, "RingDetector");
-
-    ros::NodeHandle nodeHandle;
-	ImageTransport it(nodeHandle);
-    Subscriber sub = it.subscribe("ardrone/front/image_raw", 60, imageCallback);
-    while(ros::ok()){
-		ros::spinOnce();
-		cv::waitKey(1);
-    }
-	sem_destroy(semaphore);
-    writer->release();
-    delete writer;
-	delete semaphore;
-
-    return 0;
 }
 
-void processImage( cv_bridge::CvImageConstPtr image)
+void Ring_Detector::processImage( cv_bridge::CvImageConstPtr image)
 {
     cv::Mat grad;
 	double delta = 0.0;
@@ -149,5 +56,4 @@ void processImage( cv_bridge::CvImageConstPtr image)
          	cv::circle(droneFeed, center, radius, cv::Scalar(255,0,255), 3, 8, 0);
     	}
     	imshow("Drone Feed", droneFeed);
-    	return;
 }
