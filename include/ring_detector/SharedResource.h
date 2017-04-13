8df64832cdabc310e8c5e936fffd47d6db9d2891
Hjorthen <Hjorthensebastian@gmail.com>
2017-04-13 20:15:54 +0200

    Image processors no longer process the same image constantly

--- include/ring_detector/SharedResource.h
+++ include/ring_detector/SharedResource.h
@@ -1,13 +1,15 @@
 #include <shared_mutex>
 #include <mutex>
-
-
+#include <condition_variable>
+#include <ros/console.h>
 
 
 template<typename T>
 class SharedResource{
   mutable std::shared_timed_mutex m_Mutex;
+  std::condition_variable_any m_Condition;
   T resource;
+  
  public:
   SharedResource(const T& init){
     resource = init;
@@ -16,12 +18,14 @@ class SharedResource{
   }
   T Get(){
     std::shared_lock<std::shared_timed_mutex>  lock(m_Mutex);
+    m_Condition.wait(lock);
     return resource;
   }
     
   void Set(const T& element){
     std::unique_lock<std::shared_timed_mutex> lock(m_Mutex);
     resource = element;
+    m_Condition.notify_all();
   }
 
 
