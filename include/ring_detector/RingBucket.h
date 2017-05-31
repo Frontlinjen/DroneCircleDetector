66b035e1b0d3e5e61e6f2c7b3b0684706753097f
Hjorthen <Hjorthensebastian@gmail.com>
2017-05-31 10:31:02 +0200

    Added ring bucket header

---
+++ include/ring_detector/RingBucket.h
@@ -0,0 +1,24 @@
+#include <vector>
+#include "ring_detector/MessageFormats.h"
+constexpr unsigned int grid_height = 11;
+constexpr unsigned int grid_width = 10;
+typedef std::vector<RingData*> RingBucketContainer;
+
+
+class RingBucket{
+RingBucketContainer m_buckets[grid_height][grid_width];
+
+public:
+//Inserts the ring into a bucket and takes ownership
+void Insert(RingData* data);
+
+//Gets the buckets at coordinates
+RingBucketContainer * Get(float x, float y);
+
+//Removes the entry from the container and frees ownership
+void Remove(RingData* data);
+
+//Deletes all owned RingData
+~RingBucket();
+
+};
