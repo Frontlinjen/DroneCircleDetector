a572d9bcc9580c9b9140dab04f86a4ad7076cfa3
Hjorthen <Hjorthensebastian@gmail.com>
2017-05-31 10:30:39 +0200

    Fixed copy-paste error

--- include/ring_detector/MessageFormats.h
+++ include/ring_detector/MessageFormats.h
@@ -24,23 +24,7 @@ typedef ScanResult<CircleData> CircleScanResult;
 typedef ScanResult<QRData> QRScanResult;
 
 ScanResult<QRData> QRScanResult;
-
-struct RingData{
-  char ring_number; //The ring number, if known. Calculated from QR
-  float delta_x, delta_y; //The rings position relative to the camera
-  unsigned float abs_x, abs_y, abs_z; //The rings position in world coordinates.
-  unsigned float norm_x, norm_y; //The direction of the ring 
-  struct RingData{
-  char ring_number; //The ring number, if known. Calculated from QR
-  float delta_x, delta_y; //The rings position relative to the camera
-  unsigned float abs_x, abs_y, abs_z; //The rings position in world coordinates.
-  unsigned float norm_x, norm_y; //The direction of the ring 
-  unsigned long timestamp;
-  //char possibility;
-};
-unsigned long timestamp;
-  //char possibility;
-};
+ 
 struct RingData{
   char ring_number; //The ring number, if known. Calculated from QR
   float delta_x, delta_y, delta_z; //The rings position relative to the camera
