06848f088cbb26929e6d0138380794550f833105
Senad Begovic <senadbegovic2500@hotmail.com>
2017-05-31 14:26:38 +0200

    Added circledata object to circle detection

--- src/ring_detector.cpp
+++ src/ring_detector.cpp
@@ -13,6 +13,7 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/videoio.hpp>
 #include "ring_detector/Ring_Detector.h"
+#include "ring_detector/MessageFormats.h"
 
 void Ring_Detector:: ProcessImage(const cv_bridge::CvImageConstPtr resource)
 {
@@ -51,14 +52,21 @@ void Ring_Detector:: ProcessImage(const cv_bridge::CvImageConstPtr resource)
 	double dminRadius = minRadius / 1.0;
 	double dmaxRadius = maxRadius / 1.0;
 	cv::HoughCircles(droneFeed, circles, CV_HOUGH_GRADIENT, 1, minDist, dparam1, dparam2, dminRadius, dmaxRadius);
-	for( size_t i = 0; i < circles.size(); i++ )
+	CircleScanResult * circleResult = new CircleScanResult;
+	for(std::vector<cv::Vec3f>::iterator itr = circles.begin(); itr != circles.end(); ++itr )
 	{
-		cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
+		circleResult->objects.emplace_back();
+		CircleData * data = circleResult->objects.back();
+		cv::Point center(cvRound((*itr)[0]), cvRound((*itr)[1]));
+		int radius = cvRound((*itr)[2]);
 		// draw the circle center
 		cv::circle(droneFeed, center, 3, cv::Scalar(255,255,255), -1, 8, 0);
 		// draw the circle outline
 		cv::circle(droneFeed, center, radius, cv::Scalar(255,0,255), 3, 8, 0);
+		data->radius = radius;
+		data->angle = center;
+		data->x = center.x;
+		data->y = center.y;
 	}
 	imshow("Drone Feed", droneFeed);
 	cv::waitKey(1);
