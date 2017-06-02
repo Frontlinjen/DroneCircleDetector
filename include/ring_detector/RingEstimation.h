552fbdb50c164ca71c6c5b4226f97f480c2159d9
Hjorthen <Hjorthensebastian@gmail.com>
2017-06-02 09:17:18 +0200

    Multithreaded RingEstimation

--- include/ring_detector/RingEstimation.h
+++ include/ring_detector/RingEstimation.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "MessageFormats.h"
 #include "RingBucket.h"
+#include <deque>
+#include <condition_variable>
+#include <mutex>
 
 class RingEstimation{
-  std::vector<CircleScanResult*> circleResults;
-  std::vector<QRScanResult*> scanResults;
-  RingBucket bucket;
-  bool running;
+  std::deque<CircleScanResult*> m_CircleResults;
+  std::deque<QRScanResult*> m_ScanResults;
+  std::mutex m_lock;
+  std::condition_variable m_cond;
+  RingBucket m_Bucket;
+  bool m_Running;
  public:
-
   
   //Called from thread 1
   void Recieve(CircleScanResult* result);
@@ -16,4 +20,5 @@ class RingEstimation{
   void Recieve(QRScanResult* result);
 
   void Run();
+  void inline ProcessImage(CircleScanResult* circles, QRScanResult* QR);
 };
--- src/RingEstimation.cpp
+++ src/RingEstimation.cpp
@@ -1,21 +1,36 @@
-
-
-
-
-
-
-void RingEstimation::Recieve(CircleResult* result){
-  circleResults.add(result); //Not thread safe
+#include "ring_detector/RingEstimation.h"
+void RingEstimation::Recieve(CircleScanResult* result){
+  {
+    std::lock_guard<std::mutex> lock(m_lock);
+    m_CircleResults.push_back(result); //Not thread safe
+  }
+  m_cond.notify_one();
 }
 
 void RingEstimation::Recieve(QRScanResult* result){
-  scanResults.add(result);
+  {
+    std::lock_guard<std::mutex> lock(m_lock);
+    m_ScanResults.push_back(result);
+  }
+  m_cond.notify_one();
 }
 
 void RingEstimation::Run(){
-  running = true;
-  while(running){
-    
-  }
+  m_Running = true;
+  while(m_Running){
+    CircleScanResult* circle = NULL;
+    QRScanResult* QR = NULL;
+    std::unique_lock<std::mutex> lock(m_lock);
+    m_cond.wait(lock, [this]()->bool{return m_CircleResults.size() > 0 && m_ScanResults.size() > 0 && (m_CircleResults.front()->frameID == m_ScanResults.front()->frameID);});
+circle = m_CircleResults.front();
+QR = m_ScanResults.front();
+m_CircleResults.pop_front();
+m_ScanResults.pop_front();
+m_lock.unlock(); //No reason for us to hold onto the lock anymore.
+ProcessImage(circle, QR);
+  }      
+}
 
+void RingEstimation::ProcessImage(CircleScanResult* circles, QRScanResult* QR){
+  //Process image..
 }
