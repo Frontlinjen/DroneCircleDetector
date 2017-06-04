8a72faacded1e822e9c5bc7596fa6e7775febd4a
Senad Begovic <senadbegovic2500@hotmail.com>
2017-06-04 16:46:13 +0200

    Restructed a bit of the code ande added a distance limiter to the rings

--- src/ring_detector.cpp
+++ src/ring_detector.cpp
@@ -26,12 +26,12 @@ void Ring_Detector:: ProcessImage(const Resource<cv_bridge::CvImageConstPtr> res
 		cv::namedWindow("Input feed", CV_WINDOW_NORMAL);
 
       
-		//cv::createTrackbar("minDist", "Input feed", m_default, 300, TrackbarCallback<int, 1, 300>, &minDist);
+		cv::createTrackbar("minDist", "Input feed", m_default, 300, TrackbarCallback<int, 1, 300>, &minDist);
 		
 		cv::createTrackbar("param1", "Input feed", m_default, 300, TrackbarCallback<int, 1, 300>, &param1);
 		cv::createTrackbar("param2", "Input feed", m_default, 300, TrackbarCallback<int, 1, 300>, &param2);
-		//cv::createTrackbar("minRadius", "Input feed", m_default, 300, TrackbarCallback<int, 1, 300>, &minRadius);
-		//cv::createTrackbar("maxRadius", "Input feed", m_default, 300, TrackbarCallback<int, 1, 300>, &maxRadius);
+		cv::createTrackbar("minRadius", "Input feed", m_default, 300, TrackbarCallback<int, 1, 300>, &minRadius);
+		cv::createTrackbar("maxRadius", "Input feed", m_default, 300, TrackbarCallback<int, 1, 300>, &maxRadius);
 		*m_default = 50;
 		cv::createTrackbar("minSaturation", "Input feed", m_default, 300, TrackbarCallback<int, 100, 300>, &minSaturation);
 		*m_default = 50;
@@ -40,14 +40,6 @@ void Ring_Detector:: ProcessImage(const Resource<cv_bridge::CvImageConstPtr> res
 		cv::createTrackbar("hueValue", "Input feed", m_default, 300, TrackbarCallback<int, 0, 300>, &hueValue);
 		*m_default = 10;
 		cv::createTrackbar("hueRange", "Input feed", m_default, 300, TrackbarCallback<int, 15, 300>, &hueRange);
-		*m_default = 40;
-		cv::createTrackbar("treshold", "Input feed", m_default, 300, TrackbarCallback<int, 0, 300>, &treshold);
-		*m_default = 10;
-		cv::createTrackbar("minLength", "Input feed", m_default, 300, TrackbarCallback<int, 0, 300>, &minLength);
-		*m_default = 10;
-		cv::createTrackbar("maxGap", "Input feed", m_default, 300, TrackbarCallback<int, 0, 300>, &maxGap);
-		*m_default = 15;
-		cv::createTrackbar("lineParam", "Input feed", m_default, 300, TrackbarCallback<int, 0, 300>, &lineParam);
 		initialized = true;
 	}
 	cv::Mat grad;
@@ -88,24 +80,39 @@ void Ring_Detector:: ProcessImage(const Resource<cv_bridge::CvImageConstPtr> res
 	convertScaleAbs( grad_x, abs_grad_x );
 	convertScaleAbs( grad_y, abs_grad_y );
 	cv::Sobel(droneFeed, grad_y, CV_32FC1, 0, 1, 3);*/
-	std::vector<cv::Vec4i> circles;
-	//double dminDist = minDist / 1.0;
+	std::vector<cv::Vec3f> circles;
+	double dminDist = minDist / 1.0;
 	double dparam1 = param1 / 1.0;
 	double dparam2 = param2 / 1.0;
-	//double dminRadius = minRadius / 1.0;
-	//double dmaxRadius = maxRadius / 1.0;
-	//cv::HoughCircles(hueMask, circles, CV_HOUGH_GRADIENT, 1, minDist, dparam1, dparam2, dminRadius, dmaxRadius);
-        //cvtColor(resource->image, droneFeed, CV_BGR2GRAY);
-	cv::Mat binaryImage;
-	cv::Canny(hueMask, binaryImage, dparam1, dparam2, 3);
-	cv::HoughLinesP(binaryImage, circles, 1, (CV_PI/180.0)*lineParam, treshold, minLength, maxGap);
+	double dminRadius = minRadius / 1.0;
+	double dmaxRadius = maxRadius / 1.0;
+	cv::HoughCircles(hueMask, circles, CV_HOUGH_GRADIENT, 1, minDist, dparam1, dparam2, dminRadius, dmaxRadius);
+        //cvtColor(resource->image, droneFeed, CV_BGR2GRAY); 
 	droneFeed = resource.resource->image;
 	CircleScanResult * circleResult = new CircleScanResult;
-	for(std::vector<cv::Vec4i>::iterator itr = circles.begin(); itr != circles.end(); ++itr )
+
+	for(std::vector<cv::Vec3f>::iterator itr = circles.begin(); itr != circles.end(); ++itr )
 	{
-	  cv::Vec4i line = *itr;
-	  cv::line(droneFeed, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), cv::Scalar(0,0,0), 3, CV_AA);
+		circleResult->objects.emplace_back();
+		cv::Point center(cvRound((*itr)[0]), cvRound((*itr)[1]));
+		int radius = cvRound((*itr)[2]);
+		// draw the circle center
+		cv::circle(droneFeed, center, 3, cv::Scalar(255,255,255), -1, 8, 0);
+		// draw the circle outline
+		cv::circle(droneFeed, center, radius, cv::Scalar(255,0,255), 3, 8, 0);
+		int pixels = radius*2;
+		distance = (width * constant) / pixels;
+		std::cout << "Ring-Distance: " << distance << std::endl;
+		if(distance <4000){
+			CircleData * data = &circleResult->objects.back();
+			data->radius = radius;
+			data->angle = 0;
+			data->x = center.x;
+			data->y = center.y;
+
+		}
 	}
+
 	imshow("Drone Feed", droneFeed);
 	char c = cv::waitKey(1);
 	if(c == 27){
