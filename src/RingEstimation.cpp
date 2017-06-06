eddec3dad706764c440c69a510c9275cb6dbfff4
Senad Begovic <senadbegovic2500@hotmail.com>
2017-06-06 08:37:56 +0200

    Added public methods is going to be used in the cpp file

--- src/RingEstimation.cpp
+++ src/RingEstimation.cpp
@@ -50,18 +50,22 @@ void RingEstimation::ProcessImage(CircleScanResult* circles, QRScanResult* QR){
 	CircleData *Circledata = new CircleData();
 	QRData *QRdata = new QRData();
 	time_t timev;
+	for(std::vector<QRData>::iterator itr = QR->objects.begin(); itr != QR->objects.end(); ++itr){
+		QRdata->x;
+	}
+
 	for(std::vector<CircleData>::iterator itr = circles->objects.begin(); itr != circles->objects.end(); ++itr){
 		RingData *ringData = new RingData();
 		ringData->delta_x = Circledata->x;
 		ringData->delta_y = Circledata->y;
 		//ringData->abs_x = drone->x - ringData->delta_x;
 		//ringData->abs_y = drone->y - ringData->delta_y;
-		float i_x = ringData->abs_x - QRdata->x;
+		/*float i_x = ringData->abs_x - QRdata->x;
 		float i_y = ringData->abs_y - QRdata->y;
 		if(i_x <=1 && i_x >= -1 && i_y <=1 && i_y >= -1){
 			ringData->ring_number = QRdata->ring_number;
 			ringData->distance = QRdata->distance;
-		}
+		}*/
 		ringData->timestamp = std::time(&timev);
 		/*if(RingData(ringData->abs_x, ringData->abs_y) != null){
 			ringData->viewcount = ringData->viewcount + 1;
