349e257dd96692858425136a5082af0535fa2ef0
Hjorthen <Hjorthensebastian@gmail.com>
2017-06-13 20:00:53 +0200

    Started implementing ellipse detection

---
+++ include/ring_detector/EllipseDetector.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include <opencv2/core/core.hpp>
+
+struct Arc{
+
+
+};
+
+
+
+class EllipseDetector{
+  typedef std::vector<cv::Vec4i> LineContainer;
+
+ public:
+  static std::vector<Arc> detect(const LineContainer  lines);
+};
